84be9c0c4e1312970f0bf382ef8a7d238e4d7d18
Oktapra Amtono <oktapra.amtono@gmail.com>
2022-01-12 08:57:11 +0700

    ak3: choose dtb for haptics driver during install
    * and misc cleanup
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -37,18 +37,10 @@ patch_vbmeta_flag=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-ui_print " "
-ui_print "Swipe up the screen or press any volume key first"
-
 # Keycheck
 INSTALLER=$(pwd)
 KEYCHECK=$INSTALLER/tools/keycheck
 chmod 755 $KEYCHECK
-
-keytest() {
-  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
-  return 0
-}
 
 choose() {
   #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
@@ -65,63 +57,47 @@ choose() {
   fi
 }
 
-chooseold() {
-  # Calling it first time detects previous input. Calling it second time will do what we want
-  $KEYCHECK
-  $KEYCHECK
-  SEL=$?
-  if [ "$1" == "UP" ]; then
-    UP=$SEL
-  elif [ "$1" == "DOWN" ]; then
-    DOWN=$SEL
-  elif [ $SEL -eq $UP ]; then
-    return 0
-  elif [ $SEL -eq $DOWN ]; then
-    return 1
-  else
-    ui_print "   Vol key not detected!"
-    abort "   Use name change method in TWRP"
-  fi
-}
-
-if [ -z $NEW ]; then
-  if keytest; then
-    FUNCTION=choose
-  else
-    FUNCTION=chooseold
-    ui_print " "
-    ui_print "- Vol Key Programming -"
-    ui_print "   Press Volume Up Key: "
-    $FUNCTION "UP"
-    ui_print "   Press Volume Down Key: "
-    $FUNCTION "DOWN"
-  fi
-  ui_print " "
-  ui_print "- Select Option -"
-  ui_print "  Choose which option CPU Clock you want: "
-  ui_print "  + Volume Up = OC 2.2 GHz "
-  ui_print "  - Volume Down = Non OC 1.8 GHz "
-  if $FUNCTION; then
-    NEW=true
-  else
-    NEW=false
-  fi
-else
-  ui_print "   Option specified in zipname!"
-fi
-
 ## AnyKernel boot install
 dump_boot;
+
+ui_print " "
+ui_print "  ================================= "
+ui_print "   USE VOLUME KEY TO CHOOSE OPTION  "
+ui_print "   (+) VOLUME UP & (-) VOLUME DOWN  "
+ui_print "  ================================= "
+
+# Start select Haptic driver
+if [ -z $HAPTIC ]; then
+  FUNCTION=choose
+  ui_print " "
+  ui_print "  Choose which haptic driver: "
+  ui_print "  + QPNP Haptic (For most roms) "
+  ui_print "  - QTI Haptic (For a few roms) "
+  ui_print " "
+  if $FUNCTION; then
+    HAPTIC=true
+  else
+    HAPTIC=false
+  fi
+else
+  ui_print "- Option specified in zipname! "
+fi
 
 # If the kernel image and dtbs are separated in the zip
 decompressed_image=/tmp/anykernel/kernel/Image
 compressed_image=$decompressed_image.gz
-  # Concatenate all of the dtbs to the kernel
-  if $NEW; then
-    cat $compressed_image /tmp/anykernel/dtbs/*.dtb-oc > /tmp/anykernel/Image.gz-dtb;
-  else
-    cat $compressed_image /tmp/anykernel/dtbs/*.dtb-uc > /tmp/anykernel/Image.gz-dtb;
-  fi
+# Concatenate all of the dtbs to the kernel
+if $HAPTIC; then
+  ui_print "  > QPNP Haptic selected"
+  cat $compressed_image /tmp/anykernel/dtbs/qpnp/*.dtb > /tmp/anykernel/Image.gz-dtb;
+else
+  ui_print "  > QTI Haptic selected"
+  cat $compressed_image /tmp/anykernel/dtbs/qti/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi
+
+ui_print " "
+ui_print "- NOTE: If the vibration not working or bootloop issue after flashing, please reflash this kernel & select another option Haptic driver. "
+# End select Haptic driver
 
 write_boot;
 ## end boot install
